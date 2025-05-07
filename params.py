8154df259ef563c15dc2d89f71131f5e4a056d5d
Kevin U. Antela <145849005+M1t3vK3y@users.noreply.github.com>
2025-05-07 10:00:54 +0200

    Update params.py

--- params.py
+++ params.py
@@ -6,12 +6,7 @@ import logging
 
 # Función para almacenar en caché las URLs
 urls = [
-    (st.secrets["URLS"]["URL1"],st.secrets["KEYS"]["KEY1"],st.secrets["NAMES"]["NAME1"]),
-    (st.secrets["URLS"]["URL2"],st.secrets["KEYS"]["KEY2"],st.secrets["NAMES"]["NAME2"]),
-    (st.secrets["URLS"]["URL3"],st.secrets["KEYS"]["KEY3"],st.secrets["NAMES"]["NAME3"]),
-    (st.secrets["URLS"]["URL4"],st.secrets["KEYS"]["KEY4"],st.secrets["NAMES"]["NAME4"]),
-    (st.secrets["URLS"]["URL5"],st.secrets["KEYS"]["KEY5"],st.secrets["NAMES"]["NAME5"]),
-    (st.secrets["URLS"]["URL6"],st.secrets["KEYS"]["KEY6"],st.secrets["NAMES"]["NAME6"])
+    (st.secrets["URLS"]["URL1"],st.secrets["KEYS"]["KEY1"],st.secrets["NAMES"]["NAME1"])
 ]
 # Store visibility checkboxes for each labeler
 labelers_visibility = {}
